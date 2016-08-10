c041a445855b7154f53c206ba987a1df5fdc3d3e
Paola Maristell Begazo Gonzales <paobegon1@gmail.com>
2016-08-10 09:17:23 -0500

    mmmmmm

---
+++ js/main.js
@@ -0,0 +1,53 @@
+$(document).ready(initPage);
+
+function initPage()
+{
+	$(".nav a").click(navigatePage);
+	initScrollMagic();
+}
+
+function navigatePage(evt)
+{
+	evt.preventDefault();
+	$(".nav a").removeClass('active');
+	$(this).addClass('active');
+
+	var section = $(this).attr('href');
+	var objSection = $(section);
+
+	TweenMax.to('body', 1, {
+		scrollTo:{y:objSection.position().top},
+		ease:Quart.easeOut
+	});
+	console.log(objSection.position().top);
+}
+
+function initScrollMagic()
+{
+	var controller = new ScrollMagic.Controller();
+
+	var blockTween = new TimelineMax();
+
+	blockTween.to('#sobremi', 1.5, {backgroundColor: 'red'});
+	blockTween.to('#el_pato', 1.5, {scale: 1.2});
+
+	var portafolioTween = new TimelineMax();
+
+	portafolioTween.to('#portafolio', 1.5, {backgroundColor: 'yellow'});
+	portafolioTween.to('#portafolio', 1.5, {scale: 0.8});
+
+
+	var sobreMiScene = new ScrollMagic.Scene({
+    	triggerElement: '#sobremi'
+	})
+	.setTween(blockTween)
+	.addIndicators()
+	.addTo(controller);
+
+	var portafolioMiScene = new ScrollMagic.Scene({
+    	triggerElement: '#portafolio'
+	})
+	.setTween(portafolioTween)
+	.addIndicators()
+	.addTo(controller);
+}
